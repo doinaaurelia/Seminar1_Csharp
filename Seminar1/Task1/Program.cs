3fd1c22136d9b393e4964dba003fd80cd0581aaf
Aurika <>
2022-06-09 17:37:41 +0200

    Organised folders

--- Seminar1/Task1/Program.cs
+++ Seminar1/Task1/Program.cs
@@ -1,11 +1,22 @@
-﻿/* Напишите программу, которая на вход 
-принимает число и выдаёт его квадрат (число 
-умноженное на само себя). */
+﻿/* 1. Напишите программу, которая на вход принимает два 
+числа и проверяет, является ли первое число квадратом 
+второго.
+ a = 25, b = 5 -> да 
+ a = 2, b = 10 -> нет 
+ a = 9, b = -3 -> да 
+ a = -3 b = 9 -> нет */
 
-Console.WriteLine("Введите число: ");
-int n = int.Parse(Console.ReadLine());
+ 
+Console.Write("Введите первое число ");
+int n1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int n2 = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Число: " + n);
-Console.WriteLine("Квадрат: " + n * n);
-
-
+if(n2 == n1 * n1)
+{
+    Console.WriteLine("Yes!");
+}
+else
+{
+    Console.WriteLine("Not square");
+}
---
+++ Seminar1/Task7/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Напишите программу, которая принимает на вход 
+трёхзначное число и на выходе показывает 
+последнюю цифру этого числа.
+456 -> 6 782 -> 2 918 -> 8
+  */
+
+/* int n = 1003;
+
+if(n > 99 & n < 1000)
+{
+Console.WriteLine(n);
+Console.WriteLine(n % 10);
+} 
+else
+{
+    Console.WriteLine("Not really correct");
+}
+ */
+
+ int n = 123;
+ string str = n.ToString();
+
+Console.WriteLine(n); 
+Console.WriteLine(str[2]);
+
