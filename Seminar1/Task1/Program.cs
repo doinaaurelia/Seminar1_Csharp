f433bf4c1b7899f324f6b119d4d091f76355c4de
Aurika <>
2022-06-09 12:30:42 +0200

    Organised folders

---
+++ Seminar1/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Напишите программу, которая на вход 
+принимает число и выдаёт его квадрат (число 
+умноженное на само себя). */
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Число: " + n);
+Console.WriteLine("Квадрат: " + n * n);
+
+
