71387de7d43ef27c6c13dab59ac7b036b993be96
Aurika <>
2022-06-25 12:18:55 +0300

    Task 31

---
+++ Seminar1/Task31massive/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 31: Задайте массив из 12 эл, заполенный
+случайными числами [-9, 9]. Найдите сумму отрицательных и 
+положит элементов массива.
+ */
+
+
+
+void Print(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+ int[] mass = new int[12];
+Print(mass);
+
+ for(int i = 0; i < mass.Length; i++)
+ {
+     mass[i] = new Random().Next(-9, 10);
+ }
+ 
+Print(mass);
